67e0b683c2c7d6ce81f21c68fd20f594f1a02278
Austinfranklin12 <austinalexanderfranklin12@gmail.com>
2025-10-04 21:54:11 +0200

    added

--- script.js
+++ script.js
@@ -3,13 +3,8 @@ const button = document.getElementById("button");
 if (button) {
     button.addEventListener("click", () => {
         console.log("Button clicked!");
+        window.max.outlet("Works!")
     });
     } else {
 console.error("Button with ID 'button' not found.");
-
-    if (window.max) {
-        console.log("This webpage is loaded in Max, from a jweb object");
-    } else {
-        console.log("This webpage is not loaded in Max.")
-    }
 }
